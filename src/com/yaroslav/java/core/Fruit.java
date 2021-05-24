6dbbe33de528fe5b401a3b83fbe3828769e5de36
Yar <mr.rybalkoo@mail.ru>
2021-05-24 19:09:50 +0300

    DZZZ3

---
+++ src/com/yaroslav/java/core/Fruit.java
@@ -0,0 +1,13 @@
+package com.yaroslav.java.core;
+
+abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
